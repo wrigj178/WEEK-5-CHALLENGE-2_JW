0a49db447ae31c245fe7d79758f179ddddb70db7
wrigj178 <jjazzymane@live.com>
2014-02-27 15:13:27 -0500

    Initial Commit

---
+++ CatNap/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CatNap
+//
+//  Created by Jasmine Wright on 2/27/14.
+//  Copyright (c) 2014 Jasmine Wright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
